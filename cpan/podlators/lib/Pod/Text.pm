a91755a7431c8070ca7c8b0de21995d611d9350d
Leon Timmermans <fawaka@gmail.com>
2012-04-09 10:27:53 +0200

    Fix podlators to check for unicode layers correctly
    This patch really should go through upstream first

--- cpan/podlators/lib/Pod/Text.pm
+++ cpan/podlators/lib/Pod/Text.pm
@@ -298,8 +298,8 @@ sub start_document {
     if ($$self{opt_utf8}) {
         $$self{ENCODE} = 1;
         eval {
-            my @layers = PerlIO::get_layers ($$self{output_fh});
-            if (grep { $_ eq 'utf8' } @layers) {
+            my $flag = (PerlIO::get_layers($$self{output_fh}, output => 1, details => 1))[-1];
+            if ($flag & PerlIO::F_UTF8()) {
                 $$self{ENCODE} = 0;
             }
         };
